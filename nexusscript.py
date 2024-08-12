8f101bd4a9d5bacf82cc3b4c764cea764d5c1505
Ryan Liu <62452180+yanwrld@users.noreply.github.com>
2024-08-11 17:13:59 -0700

    status updates

--- nexusscript.py
+++ nexusscript.py
@@ -1,6 +1,7 @@
 import argparse
 import requests
 import time
+import threading
 
 parser = argparse.ArgumentParser(description="calls ttp scheduler api to find appointments and sends ntfy notification")
  
@@ -19,10 +20,12 @@ ntfy_url = f"https://ntfy.sh/{ntfy_topic}"
 interval = args.interval
 
 current_timestamp = ""
+current_status = ""
 
 # Function to check slot availability
 def check_slots():
     global current_timestamp
+    global current_status
     try:
         response = requests.get(api_url)
         response.raise_for_status()  # Raise an error for bad status codes
@@ -36,22 +39,38 @@ def check_slots():
             if start_timestamp != current_timestamp:
                 # Send notification via ntfy.sh
                 message = f"Appointment on {start_timestamp}"
-                notify_response = requests.post(ntfy_url, data=message)
+                notify_response = requests.post(ntfy_url, data=message, headers={ "Priority": "5" })
 
                 if notify_response.status_code == 200:
-                    print(f"Notification sent: {message} ({time.ctime()})")
+                    console_message = f"Notification sent: {message} ({time.ctime()})"
+                    print(console_message)
                     current_timestamp = start_timestamp
+                    current_status = console_message
                 else:
                     print("Failed to send notification.")
                 
             else:
-                print(f"Appointment on {start_timestamp}, notification previously sent {time.ctime()}")
+                console_message = f"Appointment on {start_timestamp}, notification previously sent ({time.ctime()})"
+                print(console_message)
+                current_status = console_message
 
         else:
-            print(f"No slots found({time.ctime()})")
+            console_message = f"No slots found ({time.ctime()})"
+            print(console_message)
 
     except requests.RequestException as e:
         print(f"An error occurred: {e}")
+
+def status_update():
+    global current_status
+    time.sleep(15)
+    requests.post(ntfy_url, data=f"STATUS UPDATE Current time: {time.ctime()} last status {current_status}", headers={ "Priority": "1" })
+    print("Status message sent")
+    time.sleep(585)
+
+status_thread = threading.Thread(target=status_update)
+status_thread.daemon = True
+status_thread.start()
 
 while True:
     check_slots()
